c47e3fbb33d8c1953021357d5f68523106877279
ahm-rubayed <rubayedahmed04@gmail.com>
2022-11-08 08:05:59 +0600

    title and favicon added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
     </>
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed top-0">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost btn-circle">
