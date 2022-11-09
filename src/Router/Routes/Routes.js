ce34a9c03420e4c642bba52abe154c137b0de8c4
ahm-rubayed <rubayedahmed04@gmail.com>
2022-11-09 12:53:38 +0600

    Blog section added

--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Blog from "../../Pages/Blog/Blog";
 import ErrorRoute from "../../Pages/ErrorRoute/ErrorRoute";
 import About from "../../Pages/Home/About/About";
 import Home from "../../Pages/Home/Home/Home";
@@ -37,6 +38,10 @@ const router = createBrowserRouter([
             {
                 path: "/register",
                 element: <Register></Register>
+            },
+            {
+                path: "/blog",
+                element: <Blog></Blog>
             }
         ]
 
